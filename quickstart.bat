408e4249396f96db1b6f6335950dc849a82f1c81
xautlx <xautlx@hotmail.com>
2013-06-15 22:51:44 +0800

    Signed-off-by: xautlx <xautlx@hotmail.com>

--- quickstart.bat
+++ quickstart.bat
@@ -1,11 +1,15 @@
 @echo off
 set PORT=8080
 set JAVA_OPT=-Xms256m -Xmx1024m -XX:MaxPermSize=128m
+echo Prepare to clean temp work dir and restore files...
 rm -fr work
+mkdir work\webapp
+unzip showcase-1.0.0-simplified.war -d work\webapp
+copy lib\* work\webapp\WEB-INF\lib
 echo ---------------------------------------------------------------
 echo [INFO] Please wait a moment for startup finish, when you see:
 echo [INFO]   ...Started SelectChannelConnector@0.0.0.0:%PORT%...
 echo [INFO] then use Firefox to visit the following URL:
 echo [INFO] http://localhost:%PORT%/showcase
 echo ---------------------------------------------------------------
-java %JAVA_OPT% -jar showcase.war -Dport=%PORT%
+java %JAVA_OPT% -jar showcase-1.0.0-simplified.war -Dport=%PORT%
