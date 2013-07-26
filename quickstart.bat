95d566143244d740ae1795f587f05d86f10e5e11
xautlx <xautlx@hotmail.com>
2013-07-26 22:49:31 +0800

    Signed-off-by: xautlx <xautlx@hotmail.com>

--- quickstart.bat
+++ quickstart.bat
@@ -1,6 +1,6 @@
 @echo off
 set PORT=8080
-set JAVA_OPT=-Xms256m -Xmx1024m -XX:MaxPermSize=128m
+set JAVA_OPT=-Xms256m -Xmx1024m -XX:MaxPermSize=128m -Dport=%PORT% -Dspring.profiles.active=quickstart
 echo Prepare to clean temp work dir and restore files...
 rm -fr work
 mkdir work\webapp
@@ -12,4 +12,4 @@ echo [INFO]   ...Started SelectChannelConnector@0.0.0.0:%PORT%...
 echo [INFO] then use Firefox to visit the following URL:
 echo [INFO] http://localhost:%PORT%/showcase
 echo ---------------------------------------------------------------
-java %JAVA_OPT% -jar showcase-1.0.0-simplified.war -Dport=%PORT%
+java %JAVA_OPT% -jar showcase-1.0.0-simplified.war 
